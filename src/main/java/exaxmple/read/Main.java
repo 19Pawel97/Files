04d1486d087e18d41b99de583c2f981eb5b31134
19Pawel97 <19pawel9772@gmail.com>
2021-06-08 14:57:02 +0200

    example

---
+++ src/main/java/exaxmple/read/Main.java
@@ -0,0 +1,7 @@
+package exaxmple.write;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/exaxmple/write/Main.java
@@ -0,0 +1,7 @@
+package exaxmple;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
