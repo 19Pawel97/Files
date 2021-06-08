5e4a63bda6521e74f29e1cb419d3b16979eac54c
19Pawel97 <19pawel9772@gmail.com>
2021-06-08 14:58:07 +0200

    example 2.0

--- src/main/java/exaxmple/read/Main.java
+++ src/main/java/exaxmple/read/Main.java
@@ -1,7 +1,29 @@
-package exaxmple.write;
+package exaxmple.read;
+
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        String filename = "file.txt";
+        File file = new File(filename);
+
+        if(!file.exists()){
+            System.err.println("The file does not exist.");
+            return;
+        }
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/main/java/exaxmple/write/Main.java
+++ src/main/java/exaxmple/write/Main.java
@@ -1,7 +1,18 @@
-package exaxmple;
+package exaxmple.write;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        
+        String fileName = "file.txt";
+
+        try (FileWriter fileWriter = new FileWriter(new File(fileName),true)){
+            fileWriter.write("Text new line.");
+            fileWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
